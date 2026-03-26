19196d175ddff8e0b8699adde047c2aa37a72f89
Bittu kumar <bittukum59@gmail.com>
2026-03-26 23:52:28 +0530

    unique email feature added

--- 04_authentication/src/controllers/auth.controllers.js
+++ 04_authentication/src/controllers/auth.controllers.js
@@ -4,16 +4,27 @@ const jsonwebtoken = require("jsonwebtoken")
 async function registerUser(req,res){
     const {username,email,password} = req.body;
 
+    const isUserAlreadyExists = await userModel.findOne({
+        email
+    })
+
+    if(isUserAlreadyExists){
+        return res.status(409).json({
+            message: "User already exists"
+        })
+    }
+
     const user = await userModel.create({username,email,password})
 
     const token = jsonwebtoken.sign({
         id:user._id
     },process.env.JWT_SECRET) 
 
+    res.cookie("token",token) //sending token in cookie storage
+
     res.status(201).json({
         message: "user registered successfully",
-        user,
-        token
+        user
     })
 }
 
